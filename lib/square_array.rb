bc1c56e061ca11b97a832f0ea35eb177a3282e7b
Yury Spektor <yspektor78@gmail.com>
2019-11-23 22:21:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(numbers)
   counter=0
-  while counter<numbers.length {
+  while counter<numbers.length do
  #  puts numbers[counter]
     numbers[counter]=numbers[counter]*numbers[counter]
     counter +=1
-  }
+  end
 # puts numbers
   return numbers
 end
